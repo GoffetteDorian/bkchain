ed3f0a8574722c23a61ecb4293a7d3c1c08fa95b
GoffetteDorian <goffettedorian@gmail.com>
2020-03-16 16:21:24 +0100

    Routing done

--- blockchain.py
+++ blockchain.py
@@ -44,7 +44,7 @@ class Blockchain(object):
         :param sender: <str> Address of the Sender
         :param recipient: <str> Address of the Recipient
         :param amount: <int> Amount
-        :return: <int> The index of the Block that will hold this transaction 
+        :return: <int> The index of the Block that will hold this transaction
         """
 
         self.current_transaction.append({
@@ -111,12 +111,36 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
 
 
-@app.route('/mine', method=['GET'])
+@app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 
-@app.route('/transaction/new', method=['POST'])
+@app.route('/transaction/new', methods=['POST'])
 def new_transaction():
     values = request.get_json()
 
@@ -133,7 +157,7 @@ def new_transaction():
     return jsonify(response), 201
 
 
-@app.route('/chain', method=['GET'])
+@app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
         'chain': blockchain.chain,
@@ -144,3 +168,6 @@ def full_chain():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+# @ The Mining Endpoint: https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
